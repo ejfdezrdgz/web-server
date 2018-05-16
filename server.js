cdbce97ad52209fa54801fa97d782f72fdfa8c67
ejfdezrdgz <ejfdezrdgz@gmail.com>
2018-05-16 12:35:15 +0200

    Added delete button and list loading on GET requests and web load

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ var body_parser = require('body-parser');
 var app = express();
 var tasklist = [];
 
-app.use(express.static('www/tareas'));
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   fs.readFile("./www/tareas/index.html", "utf8", function (err, text) {
-    text = text.replace("[substitute]", "");
+    text = text.replace("[substitute]", loadTasks(tasklist));
     res.send(text);
   });
   console.log('Petición GET recibida correctamente');
@@ -20,13 +19,24 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   fs.readFile('./www/tareas/index.html', 'utf8', function (err, text) {
     tasklist.push({ username: req.body.user, taskname: req.body.task });
-    var newTask = loadTasks(tasklist);
-    text = text.replace("[substitute]", newTask);
+    text = text.replace("[substitute]", loadTasks(tasklist));
     console.log(tasklist);
     res.send(text);
   });
   console.log('Petición POST recibida correctamente');
 });
+
+app.get('/delete/:id?', function (req, res) {
+  tasklist.splice(req.body.taskid, 1);
+  fs.readFile('./www/tareas/index.html', 'utf8', function (err, text) {
+    text = text.replace("[substitute]", loadTasks(tasklist));
+    console.log(tasklist);
+    res.send(text);
+  });
+  console.log('Petición DELETE recibida correctamente');
+});
+
+app.use(express.static('www/tareas'));
 
 var server = app.listen(80, function () {
   console.log('Servidor web iniciado');
@@ -37,12 +47,13 @@ function loadTasks(tasklist) {
   for (var index in tasklist) {
     var newRow = `
     <tr>
-        <td>[taskid]</td>
-        <td>[username]</td>
-        <td>[taskname]</td>
+      <td>[taskid]</td>
+      <td>[username]</td>
+      <td>[taskname]</td>
+      <td><a href="/delete?id=[taskid]">Delete</a></td>
     </tr>
     `;
-    newRow = newRow.replace("[taskid]", parseInt(index) + 1);
+    newRow = newRow.split("[taskid]").join(parseInt(index) + 1);
     newRow = newRow.replace("[username]", tasklist[index].username);
     newRow = newRow.replace("[taskname]", tasklist[index].taskname);
     newTask += newRow;
